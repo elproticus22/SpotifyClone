4f324b6f0253cce9e3a94e26e4784ae50fd92955
Jhan <160279798+elproticus22@users.noreply.github.com>
2025-04-06 17:52:58 -0500

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { supabase } from "../data/supabase";
-import logo from '../src/assets/spotify-white-logo.png'
+import logo from 'src/assets/spotify-white-logo.png'
 
 const register = () => {
   const navigate = useNavigate();
